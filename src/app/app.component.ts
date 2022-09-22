eebd397de6b7d6171b4cd9b1fab96bc3d3988b3b
basma26544 <113221934+basma26544@users.noreply.github.com>
2022-09-22 12:50:44 -0400

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component, VERSION } from '@angular/core';
+import { HomeComponent } from '/home.component';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+}
