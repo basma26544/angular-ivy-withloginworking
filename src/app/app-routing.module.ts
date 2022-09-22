8817fa7e7136501083267e16b4ea80e06fe561c3
basma26544 <113221934+basma26544@users.noreply.github.com>
2022-09-22 16:15:22 -0400

    1 file modified, 3 added, 2 deleted

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home.component';
+import { HomeComponent } from 'home';
 const routes: Routes = [
  
   { path: 'Submit', component: HomeComponent },
